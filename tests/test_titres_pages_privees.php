70ce843a9ebff71bfff4ad6eb1cb28b0bf78c888
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-08-24 13:32:16 +0200

    test: verifier titres dans disposition deployee

--- tests/test_titres_pages_privees.php
+++ tests/test_titres_pages_privees.php
@@ -1,15 +1,17 @@
 <?php
 
 $racine_depot = dirname(__DIR__);
-$racine = $racine_depot . '/prive/squelettes/contenu/';
+$racine_plugins = is_dir($racine_depot . '/plugins/association-compta')
+	? $racine_depot . '/plugins'
+	: dirname($racine_depot);
 $pages = array(
-	'destinations' => array($racine_depot . '/plugins/association-compta/prive/squelettes/contenu/', 'association_compta:destination_comptable'),
-	'dons' => array($racine_depot . '/plugins/association-dons/prive/squelettes/contenu/', 'association_dons:tous_les_dons'),
-	'ventes' => array($racine_depot . '/plugins/association-ventes/prive/squelettes/contenu/', 'association_ventes:toutes_les_ventes'),
-	'ressources' => array($racine_depot . '/plugins/association-prets/prive/squelettes/contenu/', 'association_prets:ressources_titre_liste_ressources'),
-	'prets' => array($racine_depot . '/plugins/association-prets/prive/squelettes/contenu/', 'association_prets:prets_titre_liste_reservations'),
-	'plan_comptable' => array($racine_depot . '/plugins/association-compta/prive/squelettes/contenu/', 'association_compta:plan_comptable'),
-	'bilan' => array($racine_depot . '/plugins/association-compta/prive/squelettes/contenu/', 'association_compta:bilans_comptables'),
+	'destinations' => array($racine_plugins . '/association-compta/prive/squelettes/contenu/', 'association_compta:destination_comptable'),
+	'dons' => array($racine_plugins . '/association-dons/prive/squelettes/contenu/', 'association_dons:tous_les_dons'),
+	'ventes' => array($racine_plugins . '/association-ventes/prive/squelettes/contenu/', 'association_ventes:toutes_les_ventes'),
+	'ressources' => array($racine_plugins . '/association-prets/prive/squelettes/contenu/', 'association_prets:ressources_titre_liste_ressources'),
+	'prets' => array($racine_plugins . '/association-prets/prive/squelettes/contenu/', 'association_prets:prets_titre_liste_reservations'),
+	'plan_comptable' => array($racine_plugins . '/association-compta/prive/squelettes/contenu/', 'association_compta:plan_comptable'),
+	'bilan' => array($racine_plugins . '/association-compta/prive/squelettes/contenu/', 'association_compta:bilans_comptables'),
 );
 
 foreach ($pages as $page => $definition) {
@@ -21,8 +23,8 @@ foreach ($pages as $page => $definition) {
 	}
 }
 
-$formulaire_compte = file_get_contents($racine_depot . '/plugins/association-compta/formulaires/editer_asso_comptes.php');
-$contenu_compte = file_get_contents($racine_depot . '/plugins/association-compta/prive/squelettes/contenu/editer_asso_comptes.html');
+$formulaire_compte = file_get_contents($racine_plugins . '/association-compta/formulaires/editer_asso_comptes.php');
+$contenu_compte = file_get_contents($racine_plugins . '/association-compta/prive/squelettes/contenu/editer_asso_comptes.html');
 if (str_contains($formulaire_compte, "generer_url_ecrire('asso_comptes')")
 	|| !str_contains($formulaire_compte, "generer_url_ecrire('comptes')")
 	|| str_contains($contenu_compte, '#URL_ECRIRE{asso_comptes}')
@@ -31,7 +33,7 @@ if (str_contains($formulaire_compte, "generer_url_ecrire('asso_comptes')")
 	exit(1);
 }
 
-$racine_adhesions = $racine_depot . '/plugins/association-adhesions/prive/squelettes/contenu/';
+$racine_adhesions = $racine_plugins . '/association-adhesions/prive/squelettes/contenu/';
 $edit_cotisation = file_get_contents($racine_adhesions . 'edit_cotisation.html');
 $editer_cotisation = file_get_contents($racine_adhesions . 'editer_asso_cotisation.html');
 if (!str_contains($edit_cotisation, '<h1 class="grostitre"><:association_adhesions:ajout_de_cotisation:></h1>')
@@ -41,13 +43,13 @@ if (!str_contains($edit_cotisation, '<h1 class="grostitre"><:association_adhesio
 	exit(1);
 }
 
-$voir_activites = file_get_contents($racine_depot . '/plugins/association-evenements/prive/squelettes/contenu/voir_activites.html');
+$voir_activites = file_get_contents($racine_plugins . '/association-evenements/prive/squelettes/contenu/voir_activites.html');
 if (!str_contains($voir_activites, '<h1 class="grostitre">#TITRE</h1>')) {
 	fwrite(STDERR, "Le tableau de bord d'un événement n'utilise pas son titre comme H1 privé.\n");
 	exit(1);
 }
 
-$recherche_avancee = file_get_contents($racine_depot . '/plugins/association-adhesions/prive/squelettes/contenu/recherche_avancee.html');
+$recherche_avancee = file_get_contents($racine_plugins . '/association-adhesions/prive/squelettes/contenu/recherche_avancee.html');
 if (!str_contains($recherche_avancee, '<h1 class="grostitre"><:association_adhesions:recherche_avancee_titre:></h1>')
 	|| !str_contains($recherche_avancee, '#AUTORISER{adherents_menu}')) {
 	fwrite(STDERR, "La recherche avancee ne possede pas son titre et son autorisation prives.\n");
