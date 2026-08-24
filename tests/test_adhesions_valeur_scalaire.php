f7da0ef0392961544f1594262fcb0cb8fa0f5978
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-08-24 07:01:07 +0200

    fix: prefixer les filtres de squelettes Adhesions

--- tests/test_adhesions_valeur_scalaire.php
+++ tests/test_adhesions_valeur_scalaire.php
@@ -5,6 +5,13 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 require dirname(__DIR__) . '/plugins/association-adhesions/association_adhesions_fonctions.php';
 
+$squelettes = file_get_contents(dirname(__DIR__) . '/plugins/association-adhesions/prive/squelettes/contenu/adherents.html')
+	. file_get_contents(dirname(__DIR__) . '/plugins/association-adhesions/prive/squelettes/contenu/cotisations.html');
+if (str_contains($squelettes, '|scalar_val') || !str_contains($squelettes, '|association_adhesions_valeur_scalaire')) {
+	fwrite(STDERR, "Les squelettes Adhésions utilisent encore le filtre global scalar_val.\n");
+	exit(1);
+}
+
 $cas = array(
 	array(array('', array('ignore'), 'valeur'), 'defaut', 'valeur'),
 	array(array(array('a', 'b')), 'defaut', 'a,b'),
