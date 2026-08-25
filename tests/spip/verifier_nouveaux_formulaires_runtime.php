01fcd79e35e27ed47db709e1c1b2f45bcfbae698
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-08-25 02:10:35 +0200

    test: simuler un administrateur dans les CVT runtime

--- tests/spip/verifier_nouveaux_formulaires_runtime.php
+++ tests/spip/verifier_nouveaux_formulaires_runtime.php
@@ -1,11 +1,12 @@
 <?php
 if (!defined('_ECRIRE_INC_VERSION')) { exit(2); }
+$GLOBALS['visiteur_session'] = array('id_auteur' => -1, 'statut' => '0minirezo', 'webmestre' => 'oui', 'restreint' => false);
 foreach (array('partenaire', 'banniere') as $nom) {
 	include_spip('formulaires/editer_asso_' . $nom);
 	$fonction = 'formulaires_editer_asso_' . $nom . '_charger_dist';
 	$valeurs = $fonction('new', '');
 	if (!is_array($valeurs) || empty($valeurs['editable'])) {
-		fwrite(STDERR, "ECHEC formulaire $nom non editable\n");
+		fwrite(STDERR, "ECHEC formulaire $nom non editable (type=" . objet_type('asso_' . $nom, false) . ")\n");
 		exit(1);
 	}
 	echo "OK formulaire $nom editable\n";
