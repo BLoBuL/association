f3f4b5d2c2ef5a4e21e84fea83290b283d63308a
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-08-25 02:03:01 +0200

    test: verifier les autorisations de menus en runtime

---
+++ tests/spip/verifier_menus_runtime.php
@@ -0,0 +1,10 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) { exit(2); }
+include_spip('inc/autoriser');
+$qui = array('id_auteur' => -1, 'statut' => '0minirezo', 'webmestre' => 'oui', 'restreint' => false);
+$menus = array('adherents', 'cotisations', 'activites', 'benevoles', 'partenaires', 'commerce', 'ventes', 'dons', 'comptes', 'transactions', 'prets', 'bannieres', 'notifications', 'configurer_association');
+foreach ($menus as $menu) {
+	echo "TEST $menu\n";
+	$resultat = autoriser($menu . '_menu', '', 0, $qui);
+	echo 'OK ' . $menu . '=' . ($resultat ? 'oui' : 'non') . "\n";
+}
