f6e760233da1bd6e1b2f6a37df403d73b7538ce3
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-09-09 09:02:07 +0200

    Préserver les valeurs littérales des PDF sous SPIP 4.4

--- tests/test_pdf_spipdf_adaptateur.php
+++ tests/test_pdf_spipdf_adaptateur.php
@@ -12,6 +12,13 @@ function spipdf_affichage_final($contenu) {
 	return str_replace("<\2\2?", '<?', $contenu);
 }
 require dirname(__DIR__) . '/inc/association_pdf.php';
+foreach (['adhesions/association_adherents', 'evenements/association_evenements'] as $modele) {
+	[$module, $fond] = explode('/', $modele);
+	$html_modele = file_get_contents(dirname(__DIR__) . '/plugins/association-' . $module . '/prive/pdf/' . $fond . '.html');
+	if (preg_match('/#VALEUR(?!\*\*)/', $html_modele)) {
+		throw new RuntimeException('SPIP 4.4 ne doit pas supprimer le balisage littéral avant son échappement PDF');
+	}
+}
 $attaque = '<img src="https://example.test/image">Élodie & test</img>';
 $nettoye_spipdf = html_entity_decode(association_pdf_texte($attaque), ENT_NOQUOTES, 'UTF-8');
 if (str_contains($nettoye_spipdf, '<img') || html_entity_decode($nettoye_spipdf, ENT_QUOTES, 'UTF-8') !== $attaque) {
